d2cccf1b4e876e42e659be8e7ca760e477d47dcc
Glab01 <wojteklejk5@gmail.com>
2024-12-17 09:24:26 +0100

    prawie działa

---
+++ rezerwacja.php
@@ -0,0 +1,11 @@
+<?php
+ if (isset($_POST["date"])){
+    echo "Dodano rezerwację do bazy";
+
+    $mysqli = new mysqli("localhost", "root", "", "baza");
+
+    $query = "INSERT INTO REZERWACJE VALUES (DEFAULT, 1, " . $_POST["date"] . ", " . $_POST["guests"] . ", " . $_POST["telephone"]");";
+    $mysqli->query($query);
+    $mysqli->close();
+ }
+?>
